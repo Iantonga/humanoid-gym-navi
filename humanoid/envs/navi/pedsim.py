2a22db2ce88acf0209d498f7d4f47e82f296b4cc
Ian Tong <ian_tong@hotmail.com>
2024-03-22 19:48:42 +0800

    initial pedsim refactoring done, visualization working

--- humanoid/envs/navi/pedsim.py
+++ humanoid/envs/navi/pedsim.py
@@ -3,188 +3,151 @@ import random
 import math
 import numpy as np
 import rvo2
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+
 
 from groups import extract_membership
 
-def create_nav_sim(scenario=3):
-      sim = rvo2.PyRVOSimulator(1/50., 1.5, 7, 1.5, 2, 0.2, 2)
+class PedestrianSimulator(object):
 
-# Pass either just the position (the other parameters then use
-# the default values passed to the PyRVOSimulator constructor),
-# or pass all available parameters.
-# Obstacles are also supported.
-#     o1 = sim.addObstacle([(0.1, 0.1), (-0.1, 0.1), (-0.1, -0.1)])
-#      sim.processObstacles()
-      if scenario == 0:
-            a0 = sim.addAgent((0,0))
-            a1 = sim.addAgent((2, 2))
-            a2 = sim.addAgent((2.5, 2.5))
-            a3 = sim.addAgent((3, -3))
-            a4 = sim.addAgent((3.5, -3.5))
-            a5 = sim.addAgent((4.5, 7.5))
-            a6 = sim.addAgent((5, 8))
-            sim.setAgentPrefVelocity(a1, (0, -1))
-            sim.setAgentPrefVelocity(a2, (0, -1))
-            sim.setAgentPrefVelocity(a3, (0, 1))
-            sim.setAgentPrefVelocity(a4, (0, 1))
-            sim.setAgentPrefVelocity(a5, (0, -1))
-            sim.setAgentPrefVelocity(a6, (0, -1))
+      # Class containing the simulator for pedestrians
+     
+    def __init__(self):
+        self.sim = rvo2.PyRVOSimulator(1/50., 1.5, 7, 1.5, 2, 0.2, 2)
+      
+    def create_scenario(self, scenario=1):
+        if scenario == 1:
+            a0 = self.sim.addAgent((0,0))
+            self.sim.setAgentPrefVelocity(a0, (1,0))
+            self.sim.setAgentNeighborDist(a0, 10)
+            self.sim.setAgentMaxNeighbors(a0, 10)
+            self.sim.setAgentRadius(a0,0.2)
+            self.sim.setAgentTimeHorizon(a0,10)
+            self.add_group([((3,-3),3),((3,3),2)])
 
-            sim.setAgentPrefVelocity(a0, (1,0))
-            sim.setAgentNeighborDist(a0, 5)
-            sim.setAgentRadius(a0,0.25)
-            sim.setAgentTimeHorizon(a0,3.5)
+    def getNumAgents(self):
+        return self.sim.getNumAgents()
 
-      if scenario == 1:
-            a0 = sim.addAgent((0,0))
-            a1 = sim.addAgent((6, 0))
-            #a2 = sim.addAgent((6.5, 0.3))
-            sim.setAgentPrefVelocity(a1, (-1, 0))
-            sim.setAgentMaxNeighbors(a1,0)
-            #sim.setAgentPrefVelocity(a2, (-1, 0))
-            
-            sim.setAgentPrefVelocity(a0, (1,0))
-            sim.setAgentNeighborDist(a0, 10)
-            sim.setAgentRadius(a0,0.3)
-            sim.setAgentTimeHorizon(a0,10)
-
-      if scenario == 2:
-            a0 = sim.addAgent((0,0))
-            a1 = sim.addAgent((2, 0))
-            #a2 = sim.addAgent((6.5, 0.3))
-            sim.setAgentPrefVelocity(a1, (0.5, 0))
-            sim.setAgentMaxNeighbors(a1,0)
-            #sim.setAgentPrefVelocity(a2, (-1, 0))
-            
-            sim.setAgentPrefVelocity(a0, (1,0))
-            sim.setAgentNeighborDist(a0, 10)
-            sim.setAgentRadius(a0,0.3)
-            sim.setAgentTimeHorizon(a0,10)
-
-      if scenario == 3:
-            a0 = sim.addAgent((0,0))
-            direction = [1,-1]
-            for a in range (19):
-                 # if sim.getAgentPosition(a+1)[0] >= 0:
-                     #   x = random.uniform(-1,-0.5)
-                  #else:
-                    #    x = random.uniform(0.5,1)
-                  #if sim.getAgentPosition(a+1)[1] >= 0:
-                   #     y = random.uniform(-1.1, -0.6)
-                  #else:
-                  #      y = random.uniform(0.6,1.1)
-                  velocity = random.uniform(0.5,0.8)
-                  #y = math.sqrt(abs( velocity**2 - x**2)) * direction[random.randint(0,1)]
-                  sim.addAgent((random.randint(5,15), random.randint(-9,6)))
-                  x = random.uniform(0,0.8) * (-np.sign(sim.getAgentPosition(a+1)[0]))
-                  y = math.sqrt(abs( velocity**2 - x**2)) * (-np.sign(sim.getAgentPosition(a+1)[1]))
+    def add_group(self,grouplist,tightness=None):
+        velocity = random.uniform(0.5,0.8)
+        aidx = 1
+        for group in grouplist:
+            center, num = group[0], group[1]
+            for a in range(num):
+                self.sim.addAgent((center[0]+random.uniform(-0.5,0.5),center[1]+random.uniform(-0.5,0.5)))
+                if a == 0:
+                    x = random.uniform(0,0.8) * (-np.sign(self.sim.getAgentPosition(a+1)[0]))
+                    y = math.sqrt(abs(velocity**2 - x**2)) * (-np.sign(self.sim.getAgentPosition(a+1)[1]))
 
                   #sim.setAgentPrefVelocity(a+1, (x,y))
-                  sim.setAgentPrefVelocity(a+1, (x,y))
-                  sim.setAgentRadius(a+1,0.3)
-                  #sim.setAgentTimeHorizon(a+1,5)
-            
-            sim.setAgentPrefVelocity(a0, (1,0))
-            sim.setAgentNeighborDist(a0, 10)
-            sim.setAgentMaxNeighbors(a0, 10)
-            sim.setAgentRadius(a0,0.2)
-            sim.setAgentTimeHorizon(a0,10)
+                self.sim.setAgentPrefVelocity(aidx, (x,y))
+                self.sim.setAgentRadius(aidx,0.3)
+                aidx += 1
+    
+    def step(self):
+        self.sim.doStep()
+        positions = []
+        for agent in range(self.sim.getNumAgents()):
+            positions.append(self.sim.getAgentPosition(agent))
+        return positions
+
+    def set_nav_agent_pos(self,agent,pos):
+        pos =(float(pos[0][0]), float(pos[0][1]))
+        self.sim.setAgentPosition(agent,pos)
 
 
-      if scenario == 4:
-           a0 = sim.addAgent((0,0))
-           sim.setAgentPrefVelocity(a0, (1,0))
-           sim.setAgentNeighborDist(a0, 10)
-           sim.setAgentMaxNeighbors(a0, 10)
-           sim.setAgentRadius(a0,0.2)
-           sim.setAgentTimeHorizon(a0,10)
 
-      return sim
-
-def add_group(sim,grouplist,tightness=None):
-     velocity = random.uniform(0.5,0.8)
-     aidx = 1
-     for group in grouplist:
-         center, num = group[0], group[1]
-         for a in range(num):
-              sim.addAgent((center[0]+random.uniform(-0.5,0.5),center[1]+random.uniform(-0.5,0.5)))
-              if a == 0:
-                  x = random.uniform(0,0.8) * (-np.sign(sim.getAgentPosition(a+1)[0]))
-                  y = math.sqrt(abs(velocity**2 - x**2)) * (-np.sign(sim.getAgentPosition(a+1)[1]))
-
-                  #sim.setAgentPrefVelocity(a+1, (x,y))
-              sim.setAgentPrefVelocity(aidx, (x,y))
-              sim.setAgentRadius(aidx,0.3)
-              aidx += 1
+        #print(sim.getAgentPosition(0))
+        #positions = ['(%5.3f, %5.3f)' % sim.getAgentPosition(agent_no)
+        #for agent_no in (a0, a1, a2, a3)]:
+        #    print('step=%2i  t=%.3f  %s' % (step, sim.getGlobalTime(), '  '.join(positions)))
     
 
 
-def compute_nav_sim(sim):
-     sim.doStep()
-     positions = []
-     for agent in range(sim.getNumAgents()):
-           positions.append(sim.getAgentPosition(agent))
-     return positions
-
-def set_nav_agent_pos(sim,agent,pos):
-      #print("agentpos", pos)
-      pos =(float(pos[0][0]), float(pos[0][1]))
-      sim.setAgentPosition(agent,pos)
 
 #print('Simulation has %i agents and %i obstacle vertices in it.' %
  #     (sim.getNumAgents(), sim.getNumObstacleVertices()))
 
 #print('Running simulation')
 
-def animate_agents(posx,posy):
-    assert len(posx) == len(posy), "Coordinate size mismatch"
-    num_agents = len(posx)
 
-    fig, ax = plt.subplots()
-    ax.set_xlim((-10,10))
-    ax.set_ylim((-10,10))
-    e, = ax.plot(posx[0][0],
-                 posy[0][0], 'ro')
-    p, = ax.plot([posx[i+1][0] for i in range(num_agents-1)],
-                 [posy[j+1][0] for j in range(num_agents-1)], 'bo')
-    def animation_update(frame):
-        p.set_data([posx[i+1][frame] for i in range(num_agents-1)],
-                   [posy[j+1][frame] for j in range(num_agents-1)])
-        e.set_data(posx[0][frame],
-                   posy[0][frame])
-               
-        return p,
-    ani = animation.FuncAnimation(fig=fig, func=animation_update,frames=50*20,interval=20)
-    plt.show()
+    # Pass either just the position (the other parameters then use
+# the default values passed to the PyRVOSimulator constructor),
+# or pass all available parameters.
+# Obstacles are also supported.
+#     o1 = sim.addObstacle([(0.1, 0.1), (-0.1, 0.1), (-0.1, -0.1)])
+#      sim.processObstacles()
+      # if scenario == 0:
+      #       a0 = sim.addAgent((0,0))
+      #       a1 = sim.addAgent((2, 2))
+      #       a2 = sim.addAgent((2.5, 2.5))
+      #       a3 = sim.addAgent((3, -3))
+      #       a4 = sim.addAgent((3.5, -3.5))
+      #       a5 = sim.addAgent((4.5, 7.5))
+      #       a6 = sim.addAgent((5, 8))
+      #       sim.setAgentPrefVelocity(a1, (0, -1))
+      #       sim.setAgentPrefVelocity(a2, (0, -1))
+      #       sim.setAgentPrefVelocity(a3, (0, 1))
+      #       sim.setAgentPrefVelocity(a4, (0, 1))
+      #       sim.setAgentPrefVelocity(a5, (0, -1))
+      #       sim.setAgentPrefVelocity(a6, (0, -1))
 
-if __name__ == "__main__":
-    sim = create_nav_sim(4)
-    #add_group(sim, (3,3),3)
-    add_group(sim, [((3,-3),3),((3,3),2)])
-    num_agents = sim.getNumAgents()
-    print(num_agents)
-    posx = [ [] for i in range(num_agents)]
-    posy = [ [] for i in range(num_agents)]
+      #       sim.setAgentPrefVelocity(a0, (1,0))
+      #       sim.setAgentNeighborDist(a0, 5)
+      #       sim.setAgentRadius(a0,0.25)
+      #       sim.setAgentTimeHorizon(a0,3.5)
 
-    for step in range(50*20):
-        sim.doStep()
-        pos_matrix = []
-        for agent in range(num_agents):
-            aposx = sim.getAgentPosition(agent)[0]
-            aposy = sim.getAgentPosition(agent)[1]
-            posx[agent].append(aposx)
-            posy[agent].append(aposy)
-            pos_matrix.append([aposx,aposy])
-        membership = extract_membership(pos_matrix, 1)
-    
-    #print(pos_matrix)
-    #extract_membership(pos_matrix)
-    animate_agents(posx,posy)
+      # if scenario == 1:
+      #       a0 = sim.addAgent((0,0))
+      #       a1 = sim.addAgent((6, 0))
+      #       #a2 = sim.addAgent((6.5, 0.3))
+      #       sim.setAgentPrefVelocity(a1, (-1, 0))
+      #       sim.setAgentMaxNeighbors(a1,0)
+      #       #sim.setAgentPrefVelocity(a2, (-1, 0))
+            
+      #       sim.setAgentPrefVelocity(a0, (1,0))
+      #       sim.setAgentNeighborDist(a0, 10)
+      #       sim.setAgentRadius(a0,0.3)
+      #       sim.setAgentTimeHorizon(a0,10)
 
-        #print(sim.getAgentPosition(0))
-        #positions = ['(%5.3f, %5.3f)' % sim.getAgentPosition(agent_no)
-        #for agent_no in (a0, a1, a2, a3)]:
-        #    print('step=%2i  t=%.3f  %s' % (step, sim.getGlobalTime(), '  '.join(positions)))
+      # if scenario == 2:
+      #       a0 = sim.addAgent((0,0))
+      #       a1 = sim.addAgent((2, 0))
+      #       #a2 = sim.addAgent((6.5, 0.3))
+      #       sim.setAgentPrefVelocity(a1, (0.5, 0))
+      #       sim.setAgentMaxNeighbors(a1,0)
+      #       #sim.setAgentPrefVelocity(a2, (-1, 0))
+            
+      #       sim.setAgentPrefVelocity(a0, (1,0))
+      #       sim.setAgentNeighborDist(a0, 10)
+      #       sim.setAgentRadius(a0,0.3)
+      #       sim.setAgentTimeHorizon(a0,10)
+
+      # if scenario == 3:
+      #       a0 = sim.addAgent((0,0))
+      #       direction = [1,-1]
+      #       for a in range (19):
+      #            # if sim.getAgentPosition(a+1)[0] >= 0:
+      #                #   x = random.uniform(-1,-0.5)
+      #             #else:
+      #               #    x = random.uniform(0.5,1)
+      #             #if sim.getAgentPosition(a+1)[1] >= 0:
+      #              #     y = random.uniform(-1.1, -0.6)
+      #             #else:
+      #             #      y = random.uniform(0.6,1.1)
+      #             velocity = random.uniform(0.5,0.8)
+      #             #y = math.sqrt(abs( velocity**2 - x**2)) * direction[random.randint(0,1)]
+      #             sim.addAgent((random.randint(5,15), random.randint(-9,6)))
+      #             x = random.uniform(0,0.8) * (-np.sign(sim.getAgentPosition(a+1)[0]))
+      #             y = math.sqrt(abs( velocity**2 - x**2)) * (-np.sign(sim.getAgentPosition(a+1)[1]))
+
+      #             #sim.setAgentPrefVelocity(a+1, (x,y))
+      #             sim.setAgentPrefVelocity(a+1, (x,y))
+      #             sim.setAgentRadius(a+1,0.3)
+      #             #sim.setAgentTimeHorizon(a+1,5)
+            
+      #       sim.setAgentPrefVelocity(a0, (1,0))
+      #       sim.setAgentNeighborDist(a0, 10)
+      #       sim.setAgentMaxNeighbors(a0, 10)
+      #       sim.setAgentRadius(a0,0.2)
+      #       sim.setAgentTimeHorizon(a0,10)
 
---
+++ humanoid/envs/navi/visualization.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+from pedsim import PedestrianSimulator as pedsim
+
+def animate_agents(posx,posy):
+    assert len(posx) == len(posy), "Coordinate size mismatch"
+    num_agents = len(posx)
+
+    fig, ax = plt.subplots()
+    ax.set_xlim((-10,10))
+    ax.set_ylim((-10,10))
+    e, = ax.plot(posx[0][0],
+                 posy[0][0], 'ro')
+    p, = ax.plot([posx[i+1][0] for i in range(num_agents-1)],
+                 [posy[j+1][0] for j in range(num_agents-1)], 'bo')
+    def animation_update(frame):
+        p.set_data([posx[i+1][frame] for i in range(num_agents-1)],
+                   [posy[j+1][frame] for j in range(num_agents-1)])
+        e.set_data(posx[0][frame],
+                   posy[0][frame])
+               
+        return p,
+    ani = animation.FuncAnimation(fig=fig, func=animation_update,frames=50*20,interval=20)
+    plt.show()
+
+
+def visualize(scenario=1):
+    sim = pedsim()
+    sim.create_scenario(scenario)
+    #add_group(sim, (3,3),3)
+    #sim.add_group([((3,-3),3),((3,3),2)])
+    num_agents = sim.getNumAgents()
+    print(num_agents)
+    posx = [ [] for i in range(num_agents)]
+    posy = [ [] for i in range(num_agents)]
+
+    for step in range(50*20):
+        agentpos = sim.step()
+        pos_matrix = []
+        for i,pos in enumerate(agentpos):
+            posx[i].append(pos[0])
+            posy[i].append(pos[1])
+            pos_matrix.append([pos[0],pos[1]])
+        #membership = extract_membership(pos_matrix, 1)
+    #print(pos_matrix)
+    #extract_membership(pos_matrix)
+    animate_agents(posx,posy)
+
+
+if __name__ == "__main__":
+    scenario = 1
+    visualize(scenario)
